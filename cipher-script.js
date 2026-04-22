f08fd97d23a103a601064ecf1b86b778f647918e
elskeptico <128253345+elskeptico@users.noreply.github.com>
2026-04-22 15:19:03 -0700

    it almost worc

--- cipher-script.js
+++ cipher-script.js
@@ -11,21 +11,22 @@ const alphabet = [
 ];
 
 let text = 'theworldisawheelandweareallpegs';
-let newText = '';
+let newText = 'ya';
 const shift = 3;
 
 const cipher = Math.floor(Math.random() * 3);
 
 let alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let tebahpla = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+let tebahpla = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
     
-    for (i = 0; i < text.length; i++) {
-        var coded_letter = text.charAt(i);
-        var letter_index = alphabet2.indexOf(coded_letter);
-        var decoded_letter = tebahpla.charAt(letter_index);
-        newText = newText + decoded_letter;
-    }
+for (i = 0; i < text.length; i++) {
+    var coded_letter = text.charAt(i);
+    var letter_index = alphabet2.indexOf(coded_letter);
+    var decoded_letter = tebahpla.charAt(letter_index);
+    newText = newText + decoded_letter;
+}
 alert(newText);
+
 if (cipher = 0) {
     for (let i = 0; i < text.length; i++) {
         let char = text[i]; // Accessing the letter directly
