5bc1ef7ba5d81cdb537009fcbfac8fc306379f50
elskeptico <128253345+elskeptico@users.noreply.github.com>
2026-06-01 14:54:38 -0700

    cool coll cool

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 
-const releaseTime = new Date("Jun 5, 2026 00:00:00").getTime();
+const releaseTime = new Date("Jun 5, 2026 12:00:00").getTime();
 
 
 
@@ -12,25 +12,25 @@ document.getElementById('opentesters').onclick = () => dialog.showModal();
 document.getElementById('close').onclick = () => dialog.close();
 
 
-updateCountdown();
 
 function updateCountdown() {
   const now = new Date().getTime();
   const gap = releaseTime - now;
-
+  
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
+  
   const d = Math.floor(gap / day);
   const h = Math.floor((gap % day) / hour);
   const m = Math.floor((gap % hour) / minute);
   const s = Math.floor((gap % minute) / second);
-  dayEl.innerText = d;
-  hourEl.innerText = h;
-  minuteEl.innerText = m;
-  secondEl.innerText = s;
+  dayEl.innerHTML = d;
+  hourEl.innerHTML = h;
+  minuteEl.innerHTML = m;
+  secondEl.innerHTML = s;
   setTimeout(updateCountdown, 1000)
 }
 
+updateCountdown();
