b2b9cac0a7f41b87843c612dbf7febdbf86a6fcb
elskeptico <128253345+elskeptico@users.noreply.github.com>
2026-06-02 10:44:04 -0700

    VERY UNFINISHED

--- scripts/script.js
+++ scripts/script.js
@@ -26,3 +26,19 @@ function updateCountdown() {
 }
 
 updateCountdown();
+
+function copyText() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+  var link = "https://elskeptico.github.io/zombieapoc/modpack.mrpack";
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(link);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
