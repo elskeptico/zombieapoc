4e2cea6d1058a6fa7d844e77feef2490eeacbe29
elskeptico <128253345+elskeptico@users.noreply.github.com>
2026-06-05 16:55:17 -0700

    ahh

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,4 @@
-const dayEl = document.getElementById("day");
-const hourEl = document.getElementById("hour");
-const minuteEl = document.getElementById("minute");
-const secondEl = document.getElementById("second");
 
-const releaseTime = new Date("Jun 5, 2026 12:00:00").getTime();
-
-function updateCountdown() {
-  const now = new Date().getTime();
-  const gap = releaseTime - now;
-  
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  
-  const d = Math.floor(gap / day);
-  const h = Math.floor((gap % day) / hour);
-  const m = Math.floor((gap % hour) / minute);
-  const s = Math.floor((gap % minute) / second);
-  dayEl.innerHTML = d.toString;
-  hourEl.innerHTML = h.toString;
-  minuteEl.innerHTML = m.toString;
-  secondEl.innerHTML = s.toString;
-  setTimeout(updateCountdown, 1000)
-}
-
-updateCountdown();
 
 function copyText() {
   // Get the text field
